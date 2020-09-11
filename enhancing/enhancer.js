1a4193206b89b9a3e55d098994df0edd29956d3e
Kadeem140 <Beckford.kadeem@yahoo.com>
2020-09-10 21:37:40 -0400

    MVP with Passing tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,30 @@ module.exports = {
   success,
   fail,
   repair,
-  get,
 };
 
+
 function success(item) {
-  return { ...item };
+  const newEnhancementLvl = item.enhancement < 20 ? item.enhancement + 1 : 20
+  return { ...item, enhancement: newEnhancementLvl }
 }
 
 function fail(item) {
-  return { ...item };
+ 
+  let newDurability = item.durability - 1 // set the base case(numbers greater than 16)
+  if (item.enhancement < 15) {
+    newDurability = item.durability - 5
+  } // first check and reassignment
+  else if (item.enhancement === 15 || item.enhancement === 16) {
+    newDurability = item.durability - 10
+  } // second check and reassignment
+
+  return { ...item, durability: newDurability }
 }
 
+//restores durability back to 100
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 }
 }
 
-function get(item) {
-  return { ...item };
-}
+
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,41 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("Repairs item", () => {
+    it("Should set items durability to 100", () => {
+        const item = {enhancement: 10, durability: 20}
+        const repairedItem = enhancer.repair(item)
+        expect(repairedItem.durability).toBe(100)
+    })
+})
+
+describe("Enhances item", function () {
+    // on success enhancement increases by one limiting at 20
+    // on fail enhancement as e < 15 durability as d -= 5 || e === 15 e === 16 d -= 10 || d -= 1
+    it("Should succeed in enhancement", () => {
+        const item = {enhancement: 20, durability: 100}
+        const newItem = enhancer.success(item)
+        expect(newItem.enhancement).toBe(20)
+    })
+    it("Should succeed in enhancement setting enhancement to 11", () => {
+        const item = {enhancement: 10, durability: 100}
+        const newItem = enhancer.success(item)
+        expect(newItem.enhancement).toBe(11)
+    })
+    it("Should fail in enhancement setting durability to 95", () => {
+        const item = {enhancement: 10, durability: 100}
+        const newItem = enhancer.fail(item)
+        expect(newItem.durability).toBe(95)
+    })
+    it("Should fail in enhancement setting durability to 90", () => {
+        const item = {enhancement: 15, durability: 100}
+        const newItem = enhancer.fail(item)
+        expect(newItem.durability).toBe(90)
+    })
+    it("Should fail in enhancement setting durability to 99", () => {
+        const item = {enhancement: 20, durability: 100}
+        const newItem = enhancer.fail(item)
+        expect(newItem.durability).toBe(99)
+    })
+})
+
